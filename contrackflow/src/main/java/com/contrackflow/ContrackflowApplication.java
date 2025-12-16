4ff11572b2f13926e1a24d78299fc0e47eb0be73
Nedyalka Radkova <nellidimitrowa@gmail.com>
2025-12-16 20:02:57 +0200

    Initial commit: setup project with Spring Boot, MySQL, Docker and hello endpoint

---
+++ contrackflow/src/main/java/com/contrackflow/ContrackflowApplication.java
@@ -0,0 +1,13 @@
+package com.contrackflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrackflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrackflowApplication.class, args);
+	}
+
+}
